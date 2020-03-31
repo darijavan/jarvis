5dafa93a5646460d008966dae480fe796df5008d
darijavan <darijavan@gmail.com>
2020-03-31 15:17:18 +0300

    add typing

--- classes.py
+++ classes.py
@@ -1,33 +1,44 @@
+from typing import List
+
+
 class Point:
     def __init__(self, x, y):
         self.x = x
         self.y = y
 
+
 class Jarvis:
     @staticmethod
-    def orientation(p, q, r):
+    def orientation(p: Point, q: Point, r: Point):
         val = (q.y - p.y) * (r.x - q.x) - (q.x - p.x) * (r.y - q.y)
-        if val == 0 : return 0 
-        elif val > 0 : return 1
-        else : return 2
+        if val == 0:
+            return 0
+        elif val > 0:
+            return 1
+        else:
+            return 2
 
     @staticmethod
-    def envconvex(point, n):
-        if n < 3 : return
+    def envconvex(points: List[Point]):
+        n = len(points)
+
+        if n < 3:
+            return
         env = []
         l = 0
         for i in range(1, n):
-            if(point[i].x < point[l].x):
+            if(points[i].x < points[l].x):
                 l = i
         p = l
-        while True :
-            env.append(point[p])
+        while True:
+            env.append(points[p])
             q = (p + 1) % n
 
             for i in range(1, n):
-                if Jarvis.orientation(point[p], point[i], point[q]) == 2 :
+                if Jarvis.orientation(points[p], points[i], points[q]) == 2:
                     q = i
             p = q
-            if p == l : break
+            if p == l:
+                break
         for temp in env:
             print("(", temp.x, ", ", temp.y, ")")
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 from classes import Point
 from classes import Jarvis
 
-points = [Point(1,1), Point(2,2), Point(2,0), Point(2,4), Point(3,3), Point(4,2)]
-n = len(points)
+points = [
+    Point(1, 1),
+    Point(2, 2),
+    Point(2, 0),
+    Point(2, 4),
+    Point(3, 3),
+    Point(4, 2),
+]
 
-Jarvis.envconvex(points, n)
+Jarvis.envconvex(points)
